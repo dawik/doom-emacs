28864f63477024ebcd33d1b2e50323d1dbf26e67
David Wikmans <dwikmans@planview.com>
2019-01-22 22:57:13 +0100

    remove dashboard

--- init.el
+++ init.el
@@ -227,7 +227,7 @@ decrease this. If you experience stuttering, increase this.")
        :ui
        ;;deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
-       doom-dashboard    ; a nifty splash screen for Emacs
+       ;;doom-dashboard    ; a nifty splash screen for Emacs
        doom-modeline     ; a snazzy Atom-inspired mode-line
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
        evil-goggles      ; display visual hints when editing in evil
