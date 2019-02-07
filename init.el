7ace1798c12d8e0ebfb323f7e3a5c73841d1ffdb
David Wikmans <dwikmans@planview.com>
2019-02-07 21:50:09 +0100

    clean up a bit

--- init.el
+++ init.el
@@ -106,38 +106,6 @@ decrease this. If you experience stuttering, increase this.")
 )
 
 (setq doom-theme 'doom-vibrant)
-(global-unset-key (kbd "M-h"))
-(global-unset-key (kbd "M-j"))
-(global-unset-key (kbd "M-k"))
-(global-unset-key (kbd "M-l"))
-(global-unset-key (kbd "M-o"))
-(global-unset-key (kbd "M-f"))
-(global-unset-key (kbd "M-e"))
-(global-unset-key (kbd "M-i"))
-(global-unset-key (kbd "M-b"))
-(global-unset-key (kbd "M-w"))
-(global-unset-key (kbd "C-u"))
-(global-unset-key (kbd "C-c"))
-(global-set-key (kbd "C-u") 'evil-scroll-up)
-(global-set-key (kbd "M-t") 'treemacs)
-(global-set-key (kbd "M-g") 'magit-status)
-(global-set-key (kbd "M-O") 'helm-projectile-switch-project)
-(global-set-key (kbd "M-o") 'helm-projectile-find-file)
-(global-set-key (kbd "M-c") 'helm-make-projectile)
-(global-set-key (kbd "M-e") 'prettier-eslint)
-(global-set-key (kbd "C-s") 'helm-swoop-symble-pre-input)
-(global-set-key (kbd "M-h") 'windmove-left)
-(global-set-key (kbd "M-j") 'windmove-down)
-(global-set-key (kbd "M-k") 'windmove-up)
-(global-set-key (kbd "M-l") 'windmove-right)
-(global-set-key (kbd "M-b") 'ido-switch-buffer)
-(global-set-key (kbd "M-x") 'kill-buffer-and-window)
-(global-set-key (kbd "M-f") 'helm-swoop)
-(global-set-key (kbd "M-F") 'helm-multi-swoop-projectile)
-(global-set-key (kbd "M-o") 'helm-projectile-ag)
-(global-set-key (kbd "M-O") 'helm-projectile-grep)
-(global-set-key (kbd "M-w") 'save-buffer)
-(global-set-key (kbd "M-W") 'save-buffers-kill-emacs)
 
 
 
@@ -231,125 +199,6 @@ decrease this. If you experience stuttering, increase this.")
   (key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
   (key-chord-mode 1))
 
-(doom! :feature
-       ;;debugger          ; FIXME stepping through code, to help you add bugs
-       eval              ; run code, run (also, repls)
-       (evil +everywhere); come to the dark side, we have cookies
-       file-templates    ; auto-snippets for empty files
-       (lookup           ; helps you navigate your code and documentation
-        +docsets)        ; ...or in Dash docsets locally
-       snippets          ; my elves. They type so I don't have to
-       spellcheck        ; tasing you for misspelling mispelling
-       syntax-checker    ; tasing you for every semicolon you forget
-       workspaces        ; tab emulation, persistence & separate workspaces
-
-       :completion
-       ;;company           ; the ultimate code completion backend
-       helm              ; the *other* search engine for love and life
-       ;;ido              ; the other *other* search engine...
-       ;;ivy              ; a search engine for love and life
-
-       :ui
-       ;;deft              ; notational velocity for Emacs
-       doom              ; what makes DOOM look the way it does
-       ;;doom-dashboard    ; a nifty splash screen for Emacs
-       doom-modeline     ; a snazzy Atom-inspired mode-line
-       doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       evil-goggles      ; display visual hints when editing in evil
-       ;;fci               ; a `fill-column' indicator
-       hl-todo           ; highlight TODO/FIXME/NOTE tags
-       ;;modeline          ; snazzy, Atom-inspired modeline, plus API
-       nav-flash         ; blink the current line after jumping
-       ;;neotree           ; a project drawer, like NERDTree for vim
-       treemacs          ; a project drawer, like neotree but cooler
-       window-select     ; visually switch windows
-
-       :editor
-       ;;(format +onsave)  ; automated prettiness
-       ;;lispy             ; vim for lisp, for people who dont like vim
-       multiple-cursors  ; editing in many places at once
-       ;;parinfer          ; turn lisp into python, sort of
-       :lang
-       ;;assembly          ; assembly for fun or debugging
-       ;;(cc +irony +rtags); C/C++/Obj-C madness
-       ;;clojure           ; java with a lisp
-       ;;common-lisp       ; if you've seen one lisp, you've seen them all
-       ;;coq               ; proofs-as-programs
-       ;;crystal           ; ruby at the speed of c
-       ;;csharp            ; unity, .NET, and mono shenanigans
-       data              ; config/data formats
-       erlang            ; an elegant language for a more civilized age
-       ;;elixir            ; erlang done right
-       ;;elm               ; care for a cup of TEA?
-       emacs-lisp        ; drown in parentheses
-       ;;ess               ; emacs speaks statistics
-       ;;go                ; the hipster dialect
-       ;;(haskell +intero) ; a language that's lazier than I am
-       ;;hy                ; readability of scheme w/ speed of python
-       ;;idris             ;
-       ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
-       ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
-       ;;julia             ; a better, faster MATLAB
-       ;;latex             ; writing papers in Emacs has never been so fun
-       ;;ledger            ; an accounting system in Emacs
-       ;;lua               ; one-based indices? one-based indices
-       markdown          ; writing docs for people to ignore
-       ;;nim               ; python + lisp at the speed of c
-       ;;nix               ; I hereby declare "nix geht mehr!"
-       ;;ocaml             ; an objective camel
-       (org              ; organize your plain life in plain text
-        +attach          ; custom attachment system
-        +babel           ; running code in org
-        +capture         ; org-capture in and outside of Emacs
-        +export          ; Exporting org to whatever you want
-        +present)        ; Emacs for presentations
-       ;;perl              ; write code no one else can comprehend
-       ;;php               ; perl's insecure younger brother
-       ;;plantuml          ; diagrams for confusing people more
-       ;;purescript        ; javascript, but functional
-       ;;python            ; beautiful is better than ugly
-       ;;qt                ; the 'cutest' gui framework ever
-       ;;racket            ; a DSL for DSLs
-       ;;rest              ; Emacs as a REST client
-       ;;ruby              ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
-       ;;scala             ; java, but good
-       (sh +fish)        ; she sells (ba|z|fi)sh shells on the C xor
-       ;;solidity          ; do you need a blockchain? No.
-       ;;swift             ; who asked for emoji variables?
-       ;;web               ; the tubes
-       ;;vala              ; GObjective-C
-
-       ;; Applications are complex and opinionated modules that transform Emacs
-       ;; toward a specific purpose. They may have additional dependencies and
-       ;; should be loaded late.
-       :app
-       ;;(email +gmail)    ; emacs as an email client
-       ;;irc               ; how neckbeards socialize
-       ;;(rss +org)        ; emacs as an RSS reader
-       ;;twitter           ; twitter client https://twitter.com/vnought
-       ;;(write            ; emacs as a word processor (latex + org + markdown)
-       ;; +wordnut         ; wordnet (wn) search
-       ;; +langtool)       ; a proofreader (grammar/style check) for Emacs
-
-       :collab
-       ;;floobits          ; peer programming for a price
-       ;;impatient-mode    ; show off code over HTTP
-
-       :config
-
-       ;; literate config in your `doom-private-dir' whenever it changes.
-       ;;literate
-
-       ;; The default module sets reasonable defaults for Emacs. It also
-       ;; provides a Spacemacs-inspired keybinding scheme, a custom yasnippet
-       ;; library, and additional ex commands for evil-mode. Use it as a
-       ;; reference for your own modules.
-       (default +bindings +evil-commands))
-
-;;(when (require 'persp-mode nil 'noerror)
-;;  (persp-mode))
-
 (setq-default message-log-max nil)
 (setq initial-scratch-message "")
 (setq inhibit-startup-buffer-menu t)
@@ -412,10 +261,6 @@ Version 2016-06-19"
                  (setq i (1+ i)))
         (progn (setq i 100))))))
 
-(global-set-key (kbd "M-n") 'xah-next-user-buffer)
-(global-set-key (kbd "M-p") 'xah-previous-user-buffer)
-(global-set-key (kbd "C-c") 'evil-normal-state)
-(define-key global-map (kbd "RET") 'newline-and-indent)
 
 (add-hook 'after-init-hook 'global-company-mode)
 
@@ -424,8 +269,44 @@ Version 2016-06-19"
 (evil-set-initial-state 'info-mode 'normal)
 (setq evil-normal-state-modes (append evil-motion-state-modes evil-normal-state-modes))
 (setq evil-motion-state-modes nil)
-
 (define-key evil-normal-state-map (kbd "C-a") 'evil-numbers/inc-at-pt)
 (define-key evil-normal-state-map (kbd "C-x") 'evil-numbers/dec-at-pt)
-
 (define-key evil-normal-state-map (kbd "/") 'helm-swoop-without-pre-input)
+(define-key evil-insert-state-map (kbd "RET") 'newline-and-indent)
+
+(global-unset-key (kbd "M-h"))
+(global-unset-key (kbd "M-j"))
+(global-unset-key (kbd "M-k"))
+(global-unset-key (kbd "M-l"))
+(global-unset-key (kbd "M-o"))
+(global-unset-key (kbd "M-f"))
+(global-unset-key (kbd "M-e"))
+(global-unset-key (kbd "M-i"))
+(global-unset-key (kbd "M-b"))
+(global-unset-key (kbd "M-w"))
+(global-unset-key (kbd "C-u"))
+(global-unset-key (kbd "C-c"))
+
+(global-set-key (kbd "C-u") 'evil-scroll-up)
+(global-set-key (kbd "M-t") 'treemacs)
+(global-set-key (kbd "M-g") 'magit-status)
+(global-set-key (kbd "M-O") 'helm-projectile-switch-project)
+(global-set-key (kbd "M-o") 'helm-projectile-find-file)
+(global-set-key (kbd "M-c") 'helm-make-projectile)
+(global-set-key (kbd "M-e") 'prettier-eslint)
+(global-set-key (kbd "C-s") 'helm-swoop-symble-pre-input)
+(global-set-key (kbd "M-h") 'windmove-left)
+(global-set-key (kbd "M-j") 'windmove-down)
+(global-set-key (kbd "M-k") 'windmove-up)
+(global-set-key (kbd "M-l") 'windmove-right)
+(global-set-key (kbd "M-b") 'ido-switch-buffer)
+(global-set-key (kbd "M-x") 'kill-buffer-and-window)
+(global-set-key (kbd "M-f") 'ace-jump-char-mode)
+(global-set-key (kbd "M-F") 'helm-multi-swoop-projectile)
+(global-set-key (kbd "M-o") 'helm-projectile-ag)
+(global-set-key (kbd "M-O") 'helm-projectile-grep)
+(global-set-key (kbd "M-w") 'save-buffer)
+(global-set-key (kbd "M-W") 'save-buffers-kill-emacs)
+(global-set-key (kbd "M-n") 'xah-next-user-buffer)
+(global-set-key (kbd "M-p") 'xah-previous-user-buffer)
+(global-set-key (kbd "C-c") 'evil-normal-state)
